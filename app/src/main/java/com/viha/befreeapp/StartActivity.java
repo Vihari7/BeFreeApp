57a02fd5d3a98bdb8ed571b1f791c23099a73de2
VIHARI7 <dviha7@gmail.com>
2024-06-09 22:04:06 +0530

    create signup and login

--- app/src/main/java/com/viha/befreeapp/StartActivity.java
+++ app/src/main/java/com/viha/befreeapp/StartActivity.java
@@ -3,12 +3,37 @@ package com.viha.befreeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
-
+    private Button startButton;
+    private TextView linkToLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        startButton = findViewById(R.id.start_button);
+        linkToLogin = findViewById(R.id.linkToLogin);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, Signup.class);
+                startActivity(intent);
+            }
+        });
+
+
+        linkToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
