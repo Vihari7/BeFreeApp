bf92db4b81d7c9c220ff5145b018a0a8e9895140
VIHARI7 <dviha7@gmail.com>
2024-06-10 13:49:18 +0530

    create signup

--- app/src/main/java/com/viha/befreeapp/Signup.java
+++ app/src/main/java/com/viha/befreeapp/Signup.java
@@ -3,12 +3,38 @@ package com.viha.befreeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        View buttonSignup = findViewById(R.id.buttonSignup);
+        View linkToLogin = findViewById(R.id.linkToLogin);
+
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Your signup logic here
+
+                // Redirect to Login activity after signup
+                Intent intent = new Intent(Signup.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+        linkToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Signup.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
