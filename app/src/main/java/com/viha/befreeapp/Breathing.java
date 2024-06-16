6dcdb0dd231e290e0eca3fe0b20e1c4a6bcacdf5
VIHARI7 <dviha7@gmail.com>
2024-06-16 09:12:48 +0530

    update breathing activity

--- app/src/main/java/com/viha/befreeapp/Breathing.java
+++ app/src/main/java/com/viha/befreeapp/Breathing.java
@@ -7,12 +7,14 @@ import android.os.Handler;
 import android.view.View;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.Animation;
+import android.widget.TextView;
 
 public class Breathing extends AppCompatActivity {
 
     private View circleView;
     private Handler handler = new Handler();
     private boolean isExpanding = true;
+    private TextView breathingInstructions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,23 +22,30 @@ public class Breathing extends AppCompatActivity {
         setContentView(R.layout.activity_breathing);
 
         circleView = findViewById(R.id.circleView);
+        breathingInstructions = findViewById(R.id.breathingInstructions);
         startBreathingAnimation();
     }
 
     private void startBreathingAnimation() {
         final int inhaleDuration = 4000;  // 4 seconds
+        final int holdDuration = 2000; // 2 seconds
         final int exhaleDuration = 6000;  // 6 seconds
 
         handler.post(new Runnable() {
-            @Override
             public void run() {
                 if (isExpanding) {
+                    breathingInstructions.setText("Inhale");
                     animateCircle(1.0f, 1.5f, inhaleDuration);
+                    handler.postDelayed(() -> {
+                        breathingInstructions.setText("Hold");
+                    }, inhaleDuration);
+                    handler.postDelayed(this, inhaleDuration + holdDuration);
                 } else {
+                    breathingInstructions.setText("Exhale");
                     animateCircle(1.5f, 1.0f, exhaleDuration);
+                    handler.postDelayed(this, exhaleDuration);
                 }
                 isExpanding = !isExpanding;
-                handler.postDelayed(this, isExpanding ? inhaleDuration : exhaleDuration);
             }
         });
     }
