707a79518f99e01f0ca31143f300966e11387c78
VIHARI7 <dviha7@gmail.com>
2024-07-06 16:08:30 +0530

    stylish more

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:23.0.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("com.google.android.libraries.places:places:3.5.0")
+    implementation("com.google.firebase:firebase-storage:21.0.0")
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.11.0")
 }
--- app/src/main/java/com/viha/befreeapp/Profile.java
+++ app/src/main/java/com/viha/befreeapp/Profile.java
@@ -125,5 +125,4 @@ public class Profile extends AppCompatActivity {
             profilePassword.setText(updatedPassword);
         }
     }
-
 }
