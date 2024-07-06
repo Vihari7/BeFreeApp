89d60e90ca0ad185efa92d2fd3e2e73da2aeba69
VIHARI7 <dviha7@gmail.com>
2024-07-06 16:32:53 +0530

    update maps

--- app/src/main/java/com/viha/befreeapp/NearbyMedicalFacility.java
+++ app/src/main/java/com/viha/befreeapp/NearbyMedicalFacility.java
@@ -1,53 +1,32 @@
 package com.viha.befreeapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class NearbyMedicalFacility extends AppCompatActivity {
-    private EditText editTextStart;
-    private EditText editTextEnd;
+
+    private EditText editTextStart, editTextEnd;
     private Button btnGetPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nearby_medical_facility);
+
         editTextStart = findViewById(R.id.editTextStart);
         editTextEnd = findViewById(R.id.editTextEnd);
         btnGetPath = findViewById(R.id.btnGetPath);
 
         btnGetPath.setOnClickListener(v -> {
-            String startingPoint = editTextStart.getText().toString();
-            String endPoint = editTextEnd.getText().toString();
-
-            if (startingPoint.equals("") || endPoint.equals("")) {
-                Toast.makeText(this, "Please enter starting location and destination",
-                        Toast.LENGTH_SHORT).show();
-            } else {
-                getPath(startingPoint, endPoint);
-            }
+            String start = editTextStart.getText().toString();
+            String end = editTextEnd.getText().toString();
+            Intent intent = new Intent(NearbyMedicalFacility.this, MapActivity.class);
+            intent.putExtra("start", start);
+            intent.putExtra("end", end);
+            startActivity(intent);
         });
-    }
-
-    private void getPath(String startingPoint, String endPoint) {
-        try {
-            Uri uri = Uri.parse("https://www.google.com/maps/dir/?api=1&origin=" + startingPoint + "&destination=" + endPoint);
-            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-            intent.setPackage("com.google.android.apps.maps");
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-        } catch (ActivityNotFoundException exception) {
-            Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=com.google.android.apps.maps&hl=en&pli=1");
-            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-        }
     }
 }
